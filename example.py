2559eb3f728aa66cfd207a50d213bd4a9d7d34ec
HYEJIN5555 <48792202+HYEJIN5555@users.noreply.github.com>
2019-04-09 15:37:20 +0900

    Add files via upload

---
+++ example.py
@@ -0,0 +1,12 @@
+# print('hello world')
+
+a = '랄라 룰랄1 234 호호하 하'
+
+언어 = a.split(' ')
+ 
+for m in 언어 :
+    print(m) 
+
+hahaha = {'랄라룰랄': '롤롤', 1234 : '일이삼사', '호호하하' : 'ㅋ크킄'}
+for m in hahaha :
+    print(hahaha[m])
